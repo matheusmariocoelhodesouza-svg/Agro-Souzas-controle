776f56e5bb07359da91978c7781795837893a5b7
matheusmariocoelhodesouza-svg <matheusmariocoelhodesouza@gmail.com>
2026-09-13 10:06:03 -0300

    chore: atualizar versão do shell comercial

--- c360-quality-hotfix.js
+++ c360-quality-hotfix.js
@@ -1,6 +1,6 @@
 (()=>{
 'use strict';
-const BOOT_VERSION='2026.09.13-b7';
+const BOOT_VERSION='2026.09.13-b8';
 const recoveryModule='./c360-autorecovery.js';
 const styles=['./c360-premium-ui.css','./c360-product-ui.css','./c360-fiscal.css'];
 const essentialModules=[
