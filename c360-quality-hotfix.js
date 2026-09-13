61daa7f1e7d257dea195774e9daedcfb9e7c29f8
matheusmariocoelhodesouza-svg <matheusmariocoelhodesouza@gmail.com>
2026-09-13 11:57:30 -0300

    chore: publish consumable actions cache

--- c360-quality-hotfix.js
+++ c360-quality-hotfix.js
@@ -1,6 +1,6 @@
 (()=>{
 'use strict';
-const BOOT_VERSION='2026.09.13-b13';
+const BOOT_VERSION='2026.09.13-b14';
 const recoveryModule='./c360-autorecovery.js';
 const styles=['./c360-premium-ui.css','./c360-product-ui.css','./c360-enterprise-ui.css','./c360-commercial.css','./c360-fiscal.css','./c360-saas-readiness.css'];
 const essentialModules=[
