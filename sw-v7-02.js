6f253244977d8c332f45aef2a823d42a5d2adaa5
matheusmariocoelhodesouza-svg <matheusmariocoelhodesouza@gmail.com>
2026-09-13 10:25:19 -0300

    chore: cache fiscal issuance module in pwa

--- sw-v7-02.js
+++ sw-v7-02.js
@@ -1,4 +1,4 @@
-const CACHE='comando360-v7-02-hotfix19';
+const CACHE='comando360-v7-02-hotfix20';
 const STABLE_CACHE='comando360-stable-v1';
 const META_CACHE='comando360-meta-v1';
 const META_KEY='./__c360_recovery_meta__';
@@ -20,6 +20,7 @@ const CORE=[
   './c360-system-health.js',
   './c360-onboarding-entry.js',
   './c360-fiscal.js',
+  './c360-fiscal-issuance.js',
   './c360-system-health.css',
   './c360-ui-polish.css',
   './c360-premium-ui.css',
