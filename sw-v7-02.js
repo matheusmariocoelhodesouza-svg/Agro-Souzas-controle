700d32b03f1563d701cac976df91d3a4c55c4c3b
matheusmariocoelhodesouza-svg <matheusmariocoelhodesouza@gmail.com>
2026-09-13 13:50:45 -0300

    chore: refresh consumables mobile ui cache

--- sw-v7-02.js
+++ sw-v7-02.js
@@ -1,4 +1,4 @@
-const CACHE='comando360-v7-02-hotfix27';
+const CACHE='comando360-v7-02-hotfix28';
 const STABLE_CACHE='comando360-stable-v1';
 const META_CACHE='comando360-meta-v1';
 const META_KEY='./__c360_recovery_meta__';
