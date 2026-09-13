b53c1316ba8093dfb9f467de7ec1b344309520af
matheusmariocoelhodesouza-svg <matheusmariocoelhodesouza@gmail.com>
2026-09-13 10:06:50 -0300

    chore: publicar cache comercial hotfix19

--- sw-v7-02.js
+++ sw-v7-02.js
@@ -1,4 +1,4 @@
-const CACHE='comando360-v7-02-hotfix18';
+const CACHE='comando360-v7-02-hotfix19';
 const STABLE_CACHE='comando360-stable-v1';
 const META_CACHE='comando360-meta-v1';
 const META_KEY='./__c360_recovery_meta__';
