8a62e34487625c72f993577ee302070a23752379
matheusmariocoelhodesouza-svg <matheusmariocoelhodesouza@gmail.com>
2026-09-17 15:01:22 -0300

    chore: remove unnecessary REST compatibility bridge

--- c360-device-control-compat.js
+++
@@ -1,9 +0,0 @@
-(()=>{
-'use strict';
-if(typeof window.restInsert!=='function'){
- window.restInsert=async function(table,payload){
-  if(typeof rest!=='function')throw new Error('API REST indisponível.');
-  return await rest(table,'','POST',payload);
- };
-}
-})();
