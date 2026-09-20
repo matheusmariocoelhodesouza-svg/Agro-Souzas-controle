a1f38c76be4bcda9c4b1ed4b8ee5d33d98d84acd
matheusmariocoelhodesouza-svg <matheusmariocoelhodesouza@gmail.com>
2026-09-19 22:23:43 -0300

    release: build tracker 1.0.1

--- android-tracker/app/build.gradle.kts
+++ android-tracker/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "br.com.comando360.tracker"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.1"
     }
 
     buildTypes {
