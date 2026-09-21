904561640df352e2f0c271ec4012852a07f242d3
matheusmariocoelhodesouza-svg <matheusmariocoelhodesouza@gmail.com>
2026-09-21 06:47:07 -0300

    Configure stable release signing for Android tracker

--- android-tracker/app/build.gradle.kts
+++ android-tracker/app/build.gradle.kts
@@ -15,9 +15,22 @@ android {
         versionName = "1.0.4"
     }
 
+    signingConfigs {
+        create("c360Release") {
+            val keystorePath = System.getenv("C360_KEYSTORE_PATH")
+            if (!keystorePath.isNullOrBlank()) {
+                storeFile = file(keystorePath)
+            }
+            storePassword = System.getenv("C360_KEYSTORE_PASSWORD")
+            keyAlias = System.getenv("C360_KEY_ALIAS") ?: "comando360"
+            keyPassword = System.getenv("C360_KEY_PASSWORD")
+        }
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = false
+            signingConfig = signingConfigs.getByName("c360Release")
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
